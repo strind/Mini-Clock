bdca0905d0936c55578d6ec5bec216f2e00f7ef4
lihaitao <strind@gykj.wentexl.cn>
2024-08-22 16:26:21 +0800

    Mini-Clock项目初始化

---
+++ src/test/java/com/strind/miniclock/MiniClockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strind.miniclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniClockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
