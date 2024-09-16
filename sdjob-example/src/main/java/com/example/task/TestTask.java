4c59b435debe6db66c15ba556d71aaf00dc5af7c
Strind <3035954996@qq.com>
2024-09-16 16:47:19 +0800

    提供分片广播案例

--- sdjob-example/src/main/java/com/example/task/TestTask.java
+++ sdjob-example/src/main/java/com/example/task/TestTask.java
@@ -1,5 +1,6 @@
 package com.example.task;
 
+import com.miniclock.core.content.SdJobHelper;
 import com.miniclock.core.handler.annotation.SdJob;
 import org.springframework.stereotype.Component;
 
@@ -19,5 +20,28 @@ public class TestTask {
         System.out.println("这是第" + atomicInteger.incrementAndGet()+ "被调用");
     }
 
+    /**
+     * 2、分片广播任务
+     */
+    @SdJob("shardingJobHandler")
+    public void shardingJobHandler() throws Exception {
+
+        // 分片参数
+        int shardIndex = SdJobHelper.getShardIndex();
+        int shardTotal = SdJobHelper.getShardTotal();
+        System.out.println("分片参数"+shardIndex+"++++++++++++"+shardTotal);
+
+        SdJobHelper.log("分片参数：当前分片序号 = {}, 总分片数 = {}", shardIndex, shardTotal);
+
+        // 业务逻辑
+        for (int i = 0; i < shardTotal; i++) {
+            if (i == shardIndex) {
+                SdJobHelper.log("第 {} 片, 命中分片开始处理", i);
+            } else {
+                SdJobHelper.log("第 {} 片, 忽略", i);
+            }
+        }
+
+    }
 
 }
